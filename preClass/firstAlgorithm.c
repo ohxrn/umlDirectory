349c36ea97bd5c0be2a4bb1813cc39ec8e15b671
Jack Ohrn <jackohrn1@gmail.com>
2024-08-24 20:25:49 -0400

    tests

---
+++ preClass/firstAlgorithm.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    /*skdink*/
+    // hi//
+    char letter = 'Y';
+    int tester = 2;
+    printf("Hello World!\n \n today I turn %d, and it is day letter %c", tester, letter);
+    printf("Hello yuy!\n");
+    printf("how r u?\n");
+
+    return 0;
+}
